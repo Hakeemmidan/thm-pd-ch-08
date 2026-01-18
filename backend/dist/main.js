48914bd00ab3ffdc5dbd8f81fb424c1d5a57475c
Abdulhakeem Almidan <Hakeemmidan@gmail.com>
2026-01-19 01:29:39 +0300

    Adjust dev cors origin

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -6,7 +6,7 @@ const common_1 = require("@nestjs/common");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
-        origin: ['http://localhost:3000'],
+        origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'http://localhost:3003'],
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         credentials: true,
     });
